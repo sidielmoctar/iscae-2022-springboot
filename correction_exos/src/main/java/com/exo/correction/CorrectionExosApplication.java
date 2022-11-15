44a8d75d4f92347373dda3c7e523c23d36059c68
SIDI <a.sidielmoctar@gmail.com>
2022-11-15 12:19:51 +0000

    la correction des 6 exos

---
+++ correction_exos/src/main/java/com/exo/correction/CorrectionExosApplication.java
@@ -0,0 +1,13 @@
+package com.exo.correction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectionExosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectionExosApplication.class, args);
+	}
+
+}
